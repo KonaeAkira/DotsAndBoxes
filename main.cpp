bdcd2eb66e98f8daff5d5cb50cc8a0c18a1939d3
Konae Akira <konae.akira@gmail.com>
2017-08-20 20:15:30 +0700

    Minor change

--- main.cpp
+++ main.cpp
@@ -4,33 +4,37 @@
 //#include "engine.h"
 using namespace std;
 
+board game(3, 3);
+
+void random_move()
+{
+	int m = game.height(), n = game.width();
+	generate_move:
+	int x = rand() % (n * (m + 1) + m * (n + 1));
+	if (game.check_valid(x))
+		game.move(x);
+	else
+		goto generate_move;
+}
+
 int main()
 {
-	board game(3, 3);
 	game.draw();
 	while (!game.end)
 	{
-		int m = game.height(), n = game.width();
 		if (!game.turn)
 		{
 			// PC
-			generate_move:
-			int x = rand() % (n * (m + 1) + m * (n + 1));
-			if (game.check_valid(x))
-				game.move(x);
-			else
-				goto generate_move;
+			random_move();
 		}
 		else
 		{
 			int x;
 			printf("Enter move: ");
-			get_move:
 			scanf("%d", &x);
 			if (game.check_valid(x))
 				game.move(x);
-			else
-				goto generate_move;
+			else random_move();
 		}
 		game.draw();
 	}
